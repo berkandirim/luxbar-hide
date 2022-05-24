5ea881e1dcb64cb2bc343ba40206f5477d98590e
Dirim, Berkan <Berkan.Dirim@adidas.com>
2022-05-24 12:26:00 +0200

    update dependencies

--- dist/luxbar-hide.js
+++ dist/luxbar-hide.js
@@ -1,26 +1,39 @@
-(function(window) {
-    var barName = 'luxbar';
-    var classes = {
-        static    : barName + '-static',
-        fixed     : barName + '-fixed'
-    };
-    
-    var previousScroll = 0;
-    var navbar = document.getElementById('luxbar'),
-        navClasses = navbar.classList;
+/**
+ * Luxbar Hide plugin
+ * @author Berkan Dirim <berkandirim@gmail.com>
+ */
 
-    window.addEventListener('scroll', function(e){
-        var currentScroll = window.scrollY;
-        var isDown = currentScroll > previousScroll;
+(function (factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module depending on jQuery.
+    define(['jquery'], factory);
+  } else {
+    // No AMD. Register plugin with global jQuery object.
+    factory(jQuery, window);
+  }
+})(function (jQuery, window) {
+  var barName = 'luxbar';
+  var classes = {
+    static: barName + '-static',
+    fixed: barName + '-fixed',
+  };
 
-        if ( isDown ){
-            navClasses.remove(classes.fixed);
-            navClasses.add(classes.static);
-        } else if ( !isDown ){
-            navClasses.remove(classes.static); 
-            navClasses.add(classes.fixed);
-        }
-        
-        previousScroll = currentScroll;
-    });
-})(window);
+  var previousScroll = 0;
+  var navbar = document.getElementById('luxbar'),
+    navClasses = navbar.classList;
+
+  document.addEventListener('scroll', function (e) {
+    var currentScroll = window.scrollY;
+    var isDown = currentScroll > previousScroll;
+
+    if (isDown) {
+      navClasses.remove(classes.fixed);
+      navClasses.add(classes.static);
+    } else if (!isDown) {
+      navClasses.remove(classes.static);
+      navClasses.add(classes.fixed);
+    }
+
+    previousScroll = currentScroll;
+  });
+})(jQuery, window);
